fc3f15ff12aab9a65faff90eac67d1b7e7c5e117
Joseph D Oronto-Pratt <67449339+Joseph2718@users.noreply.github.com>
2023-12-03 19:42:20 -0500

    Update match.py
    Finished

--- assignment2/match.py
+++ assignment2/match.py
@@ -1,41 +1,101 @@
 import numpy as np
 from typing import List, Tuple
+from collections import deque
 
-def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List[Tuple]:
+def adjust_scores_for_preferences(scores: np.ndarray, genders: List[str], preferences: List[str]) -> np.ndarray:
     """
-    TODO: Implement Gale-Shapley stable matching!
-    :param scores: raw N x N matrix of compatibility scores. Use this to derive a preference rankings.
-    :param gender_id: list of N gender identities (Male, Female, Non-binary) corresponding to each user
-    :param gender_pref: list of N gender preferences (Men, Women, Bisexual) corresponding to each user
-    :return: `matches`, a List of (Proposer, Acceptor) Tuples representing monogamous matches
+    Adjust scores based on gender identities and preferences.
+    Sets the score to 0 for incompatible gender-preference combinations.
+    """
+    gender_map = {"Male": "Men", "Female": "Women", "Nonbinary": "Bisexual"}
+    for i in range(len(scores)):
+        for j in range(len(scores[i])):
+            # Set score to 0 if preferences don't match
+            if preferences[i] != "Bisexual" and (gender_map[genders[j]] != preferences[i]):
+                scores[i][j] = 0
+    return scores
 
-    Some Guiding Questions/Hints:
-        - This is not the standard Men proposing & Women receiving scheme Gale-Shapley is introduced as
-        - Instead, to account for various gender identity/preference combinations, it would be better to choose a random half of users to act as "Men" (proposers) and the other half as "Women" (receivers)
-            - From there, you can construct your two preferences lists (as seen in the canonical Gale-Shapley algorithm; one for each half of users
-        - Before doing so, it is worth addressing incompatible gender identity/preference combinations (e.g. gay men should not be matched with straight men).
-            - One easy way of doing this is setting the scores of such combinations to be 0
-            - Think carefully of all the various (Proposer-Preference:Receiver-Gender) combinations and whether they make sense as a match
-        - How will you keep track of the Proposers who get "freed" up from matches?
-        - We know that Receivers never become unmatched in the algorithm.
-            - What data structure can you use to take advantage of this fact when forming your matches?
-        - This is by no means an exhaustive list, feel free to reach out to us for more help!
+def create_preference_lists(scores: np.ndarray) -> List[List[int]]:
     """
-    matches = [()]
-    return matches
+    Create preference lists for each user based on scores.
+    Higher scores imply higher preference.
+    """
+    return [list(np.argsort(-scores[i])) for i in range(len(scores))]
+
+def gale_shapley_algorithm(scores: np.ndarray, genders: List[str], preferences: List[str]) -> Tuple[List[Tuple], List[List[int]]]:
+    """
+    Implement the Gale-Shapley algorithm to find stable matches.
+    """
+    adjusted_scores = adjust_scores_for_preferences(scores, genders, preferences)
+    preference_lists = create_preference_lists(adjusted_scores)
+
+    n = len(scores)
+    proposers = deque(range(n // 2))  # First half as proposers
+    receivers = set(range(n // 2, n))  # Second half as receivers
+
+    proposals_made = [set() for _ in range(n)]
+    matches = [None] * n
+
+    while proposers:
+        proposer = proposers.popleft()
+        for receiver in preference_lists[proposer]:
+            if receiver not in proposals_made[proposer] and receiver in receivers:
+                proposals_made[proposer].add(receiver)
+                if matches[receiver] is None:
+                    matches[proposer] = receiver
+                    matches[receiver] = proposer
+                    break
+                else:
+                    current_match = matches[receiver]
+                    if preference_lists[receiver].index(proposer) < preference_lists[receiver].index(current_match):
+                        matches[proposer] = receiver
+                        matches[receiver] = proposer
+                        proposers.append(current_match)
+                        break
+
+    # Ensure each participant is included in the matches list
+    for receiver in receivers:
+        if matches[receiver] is None:
+            proposer = next(p for p in proposers if receiver in preference_lists[p])
+            matches[proposer] = receiver
+            matches[receiver] = proposer
+
+    return [(i, matches[i]) for i in range(n // 2)], preference_lists
+
+def is_stable_match(matches, preference_lists):
+    for a, b in matches:
+        index_b_in_a = preference_lists[a].index(b)
+        for preferred in preference_lists[a][:index_b_in_a]:
+            current_partner_of_preferred = next((x for x, y in matches if y == preferred), None)
+            if current_partner_of_preferred is not None:
+                if preference_lists[preferred].index(a) < preference_lists[preferred].index(current_partner_of_preferred):
+                    return False
+
+        index_a_in_b = preference_lists[b].index(a)
+        for preferred in preference_lists[b][:index_a_in_b]:
+            current_partner_of_preferred = next((x for x, y in matches if y == preferred), None)
+            if current_partner_of_preferred is not None:
+                if preference_lists[preferred].index(b) < preference_lists[preferred].index(current_partner_of_preferred):
+                    return False
+
+    return True
+
+def run_matching(scores: List[List[float]], gender_id: List[str], gender_pref: List[str]) -> Tuple[List[Tuple], List[List[int]]]:
+    scores_array = np.array(scores)
+    return gale_shapley_algorithm(scores_array, gender_id, gender_pref)
 
 if __name__ == "__main__":
     raw_scores = np.loadtxt('raw_scores.txt').tolist()
     genders = []
     with open('genders.txt', 'r') as file:
         for line in file:
-            curr = line[:-1]
-            genders.append(curr)
+            genders.append(line.strip())
 
     gender_preferences = []
     with open('gender_preferences.txt', 'r') as file:
         for line in file:
-            curr = line[:-1]
-            gender_preferences.append(curr)
+            gender_preferences.append(line.strip())
 
-    gs_matches = run_matching(raw_scores, genders, gender_preferences)
+    gs_matches, preference_lists = run_matching(raw_scores, genders, gender_preferences)
+    print("Matches:", gs_matches)
+    print("Stable Match:", is_stable_match(gs_matches, preference_lists))
